9bd8f2765120c41eb40a2ba8239e72934dc90627
ADMIN\mmoffett18 <marissamoffett@gmail.com>
2016-11-02 14:46:59 -0700

    First Test Commit

---
+++ TextMuse/Test1/Test1/Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using Xamarin.UITest.Android;
+
+namespace Test1
+{
+    [TestFixture(Platform.Android)]
+    public class Test
+    {
+        AndroidApp app;
+        public Test()
+        {
+
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+                 app = ConfigureApp.Android
+                .ApkFile ("/data/app/com.laloosh.textmuse.app-1/base.apk").StartApp();
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Screenshot("First screen.");
+            Assert.Pass();
+        }
+    }
+}
+
